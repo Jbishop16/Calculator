ed9417dab57fb555746d563a142f5a7291a9a01f
Jbishop16 <Jrbishop16@yahoo.com>
2016-06-17 18:26:58 -0400

    Add files via upload
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int subtract(int a, int b)
+        {
+            if (a < b)
+                return 0;
+            return a - b;
+        }
+
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public double expo(int a, int b)
+        {
+            return a ^ b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            var result = calc.add(75896, 45674);
+            Console.WriteLine(result); 
+        }
+    }
+}
